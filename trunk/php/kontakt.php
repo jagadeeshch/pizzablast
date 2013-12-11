9107d0bdaf588f6051d0826dca4dd456cbc8365c
to_be@gmx.de <to_be@gmx.de@7c536773-10e4-a68f-3034-09016fad5d5b>
2013-12-11 08:23:19 +0000

    Modifikationen Kontaktformular und Weiterleitungsseite

--- trunk/php/kontakt.php
+++ trunk/php/kontakt.php
@@ -5,28 +5,35 @@
 	$emailBetreff = 'Pizza-Blast Kontakt';
 	$webmaster = 'to_be@gmx.de';
 	
-	/* Emailform Daten */
+/* Emailform Daten */
 
 	$Name = $_POST['Name'];
 	$Email = $_POST['Email'];
 	$Betreff = $_POST['Betreff'];
 	$Nachricht = $_POST['Nachricht'];
 	
-	
 	$body = <<<EOD
-		Name: $Name
-		Email: $Email 
-		Betreff: $Betreff 
-		Nachricht: $Nachricht 
-	EOD;
-	
-	$headers = "From: $Name\r\n";
+Name: $Name
+Email: $Email 
+Betreff: $Betreff 
+Nachricht: $Nachricht 
+EOD;
+
+if($Name&&$Email&&$Betreff&&$Nachricht) 
+{
+	$headers = "From: $Email\r\n";
 	$headers .= "Content-type: text/html\r\n";
 	$headers = mail($webmaster, $emailBetreff, $body);
-	
-	
 	/* Antwort */
+	header("location:../support_success.html");
+	
+}
+else
+{
+	header("location:../support_typeerror.html");
+}
+	
 
-header("location:support_success.html");
+
 
 ?>
