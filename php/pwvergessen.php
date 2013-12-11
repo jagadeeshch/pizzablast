79221e907da4211911691f426d520d9b593655ec
to_be <to_be@gmx.de>
2013-12-11 09:25:26 +0000

    Aktivierung der "Passwort vergessen" Funktion; Funktion getestet, Weiterleitungsseiten eingefügt;

--- php/pwvergessen.php
+++ php/pwvergessen.php
@@ -1,20 +1,21 @@
 <?php
-$verbindung = mysql_connect("localhost","root","")
+$verbindung = mysql_connect("localhost","web53","lostinfulda")
 or die ("Verbindung zur Datenbank konnte nicht hergestellt werden");
-mysql_select_db ("") or die ("Datenbank konnte nicht ausgewählt werden");
+mysql_select_db ("usr_web53_1") or die ("Datenbank konnte nicht ausgewählt werden");
 
 $email = $_POST["email"];
-$webmaster = '';
+$webmaster = 'to_be@gmx.de';
 $emailBetreff = 'Ihre Zugangsdaten fuer Pizza-Blast';
 
-$datenabfrage = "SELECT id, email, password FROM customer WHERE email LIKE '$email' LIMIT 1";
+$datenabfrage = "SELECT * FROM customer WHERE email LIKE '$email' LIMIT 1";
 $daten = mysql_query($datenabfrage);
 $inhalt = mysql_fetch_object($daten);
+
 
 $body = <<<EOD
 Anbei erhalten Sie Ihre Kundennummer und Ihr Passwort fuer Pizza-Blast!
 
-Ihre Kundennummer lautet: $inhalt->id
+Ihre Kundennummer lautet: $inhalt->customerid
 Ihr Passwort lautet: $inhalt->password
 EOD;
 
@@ -25,23 +26,23 @@ if($email)
 	$ergebnis = mysql_query($abfrage);
 	$row = mysql_fetch_object($ergebnis); 
 	*/
+	
 	if($inhalt->email == $email)
     {
 		$headers = "From: $webmaster\r\n";
 		$headers .= "Content-type: text/html\r\n";
 		$headers = mail($email, $emailBetreff, $body);
-    	header("location:pwvergessen_erfolgreich.html");
+    	header("location:../pwvergessen_erfolgreich.html");
 	}
 	else
 	{
-		header("location:pwvergessen_nosuccess.html");
+		header("location:../pwvergessen_nosuccess.html");
 	}
 }
 else
 {
-	header("location:pwvergessen_typeerror.html");
+	header("location:../pwvergessen_typeerror.html");
 }
-	
 		
 
 ?>
