893cb080882cafc77ab31d8a904bc19fa522a1cc
to_be <to_be@gmx.de>
2013-12-11 09:44:49 +0000

    Aktivierung Login Funktion; Funktion getestet; Weiterleitungsseiten eingefügt;

--- php/login.php
+++ php/login.php
@@ -1,32 +1,32 @@
 <?php
 session_start();
 
-$verbindung = mysql_connect("localhost","root","")
+$verbindung = mysql_connect("localhost","web53","lostinfulda")
 or die ("Verbindung zur Datenbank konnte nicht hergestellt werden");
-mysql_select_db ("") or die ("Datenbank konnte nicht ausgewählt werden");
+mysql_select_db ("usr_web53_1") or die ("Datenbank konnte nicht ausgewählt werden");
 
 $email = $_POST["email"];
 $password = $_POST["password"];
 
 if($email&&$password)
 {
-	$abfrage = "SELECT id, email, password FROM customer WHERE email LIKE '$email' LIMIT 1";
+	$abfrage = "SELECT * FROM customer WHERE email LIKE '$email' LIMIT 1";
 	$ergebnis = mysql_query($abfrage);
 	$row = mysql_fetch_object($ergebnis);
 	
-	if($row->password == $password && $row->email == $email)
+	if($row->email == $email && $row->password == $password)
     {
-		$_SESSION["id"] = $row->id;
-    	header("location:login_success.html");
+		$_SESSION["id"] = $row->customerid;
+    	header("location:../login_success.html");
     }
 	else
     {
-		header("location:login_nosuccess.html");
+		header("location:../login_nosuccess.html");
     }
 }
 else
 {
-	header("location:login_typeerror.html");
+	header("location:../login_typeerror.html");
 }
 
 
